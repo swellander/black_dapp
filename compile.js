219e2647456986df00f3b0b810b546d364f6a40b
Sam Wellander <samwellander@gmail.com>
2018-07-25 14:53:55 -0400

    precompile contract and write some tests

--- compile.js
+++ compile.js
@@ -6,3 +6,5 @@ const blackDappPath = path.resolve(__dirname, 'contracts', 'blackdapp.sol');
 const source = fs.readFileSync(blackDappPath, 'utf8');
 
 const compiledBlackDapp = solc.compile(source, 1).contracts[':Blackdapp'];
+
+fs.writeFileSync('./compiledBlackDapp.json', JSON.stringify(compiledBlackDapp));
