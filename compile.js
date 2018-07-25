42f16325e1420bb6dbefa85c200f1c1a8024f3c0
Sam Wellander <samwellander@gmail.com>
2018-07-25 10:28:21 -0400

    npm and solidity compiler

---
+++ compile.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const solc = require('solc');
+
+const blackDappPath = path.resolve(__dirname, 'contracts', 'blackdapp.sol');
+const source = fs.readFileSync(blackDappPath, 'utf8');
+
+const compiledBlackDapp = solc.compile(source, 1).contracts[':Blackdapp'];
